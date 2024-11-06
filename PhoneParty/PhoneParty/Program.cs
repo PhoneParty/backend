4f21d39cf0e9917a304237a7b7709bae403940dc
Kirill <Vsadnikofhell@mail.ru>
2024-11-06 18:38:42 +0500

    start app

--- PhoneParty/PhoneParty/Program.cs
+++ PhoneParty/PhoneParty/Program.cs
@@ -1,6 +1,32 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+using Microsoft.AspNetCore;
 
-app.MapGet("/", () => "Hello World!");
+public class Programm
+{
+    public static void Main(string[] args)
+    {
+        BuildWebHost(args).Run();
+    }
 
-app.Run();
+    public static IWebHost BuildWebHost(string[] args)
+    {
+        return WebHost
+            .CreateDefaultBuilder(args)
+            .UseStartup<Startup>()
+            .Build();
+    }
+}
+
+public class Startup
+{
+    public void ConfigureServices(IServiceCollection services)
+    {
+        
+    }
+
+    public void Configure(IApplicationBuilder app, IHostEnvironment env)
+    {
+        if (env.IsDevelopment())
+            app.UseDeveloperExceptionPage();
+        app.Run(async context => await context.Response.WriteAsync("Hello World!"));
+    }
+}
