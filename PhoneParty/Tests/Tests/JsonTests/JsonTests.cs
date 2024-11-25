f429b85ef6c755af8dfe0e1735d748c8c635d821
Kkeie <espitsyn74@gmail.com>
2024-11-25 18:40:31 +0500

    json tests added

---
+++ PhoneParty/Tests/Tests/JsonTests/JsonTests.cs
@@ -0,0 +1,23 @@
+﻿using System.Text.Json;
+using Domain.WhoAmI;
+using NUnit.Framework;
+
+namespace PhonePartyTests.Tests.JsonTests;
+
+
+[TestFixture]
+public class JsonTests
+{
+    [TestCase]
+    public void CheckNames()
+    {
+        var jsonContent = File.ReadAllText("Heroes.json");
+        var heroData = JsonSerializer.Deserialize<HeroRepository.HeroData>(jsonContent);
+        var heroes = heroData!.Characters;
+        var names = Directory.EnumerateFiles(@"..\..\..\..\UserInterface\wwwroot\Characters").Select(Path.GetFileName);
+        foreach (var hero in heroes)
+            Assert.That(names.Contains(hero.ImgName), "Error: Jpeg name " + hero.ImgName + "Character name " + hero.Name);
+        
+    }
+    
+}
