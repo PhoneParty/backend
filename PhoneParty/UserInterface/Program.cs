51688eb1a48d3ffb3f8f0fedfd9adc063e50a35b
Kirill <Vsadnikofhell@mail.ru>
2024-11-26 18:48:05 +0500

    HTTPS

--- PhoneParty/UserInterface/Program.cs
+++ PhoneParty/UserInterface/Program.cs
@@ -2,8 +2,15 @@ using PhoneParty;
 using UserInterface;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.WebHost.ConfigureKestrel(options =>
+{
+    options.ListenAnyIP(80);
+    options.ListenAnyIP(443, listenOptions => listenOptions.UseHttps("/etc/letsencrypt/live/www.phoneparty.fun/fullchain.pem",
+        "/etc/letsencrypt/live/www.phoneparty.fun/privkey.pem"));
+});
 var start = new Startup();
 start.ConfigureServices(builder.Services);
 var app = builder.Build();
+app.UseHttpsRedirection();
 start.Configure(app);
 app.Run();
