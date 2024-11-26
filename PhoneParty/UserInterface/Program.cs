3330797e6695d4701c38cde900bfb5caab85fdfa
Kirill <Vsadnikofhell@mail.ru>
2024-11-26 20:10:06 +0500

    new path for certs

--- PhoneParty/UserInterface/Program.cs
+++ PhoneParty/UserInterface/Program.cs
@@ -5,8 +5,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.WebHost.ConfigureKestrel(options =>
 {
     options.ListenAnyIP(80);
-    options.ListenAnyIP(443, listenOptions => listenOptions.UseHttps("/etc/letsencrypt/live/www.phoneparty.fun/fullchain.pem",
-        "/etc/letsencrypt/live/www.phoneparty.fun/privkey.pem"));
+    options.ListenAnyIP(443, listenOptions => listenOptions.UseHttps("/etc/letsencrypt/archive/www.phoneparty.fun/fullchain2.pem",
+        "/etc/letsencrypt/archive/www.phoneparty.fun/privkey2.pem"));
 });
 var start = new Startup();
 start.ConfigureServices(builder.Services);
