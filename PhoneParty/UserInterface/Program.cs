02610ac9b7e694a6c2420af6ae1e81357eda1cd9
belozerov-yaroslav <jarblz@yandex.ru>
2024-12-02 19:50:57 +0500

    Добавил настройки сборки, чтобы при дебаг сборке программа не пыталась поднимать https

--- PhoneParty/UserInterface/Program.cs
+++ PhoneParty/UserInterface/Program.cs
@@ -2,15 +2,19 @@ using PhoneParty;
 using UserInterface;
 
 var builder = WebApplication.CreateBuilder(args);
+#if !DEBUG
 builder.WebHost.ConfigureKestrel(options =>
 {
     options.ListenAnyIP(80);
     options.ListenAnyIP(443, listenOptions => listenOptions.UseHttps("/etc/letsencrypt/live/www.phoneparty.fun/certificate.pfx",
         "certPassword"));
 });
+#endif
 var start = new Startup();
 start.ConfigureServices(builder.Services);
 var app = builder.Build();
+#if !DEBUG
 app.UseHttpsRedirection();
+#endif
 start.Configure(app);
 app.Run();
